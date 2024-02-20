1c659fbec1c82f94ecfd28fad81c90b96821ffb5
Chillax Zoom <159781605+chillaxzoom@users.noreply.github.com>
2024-02-20 16:21:10 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,8 +185,8 @@ document.querySelectorAll('.zoom-container').forEach((container) => {
 
   // Event listener for select change
   document.getElementById('movieType').addEventListener('change', function() {
-    var containerDiv = document.querySelector('.container');
-  var tvSeriesSection = document.querySelector('.tv-series');  
+var tvSeriesSection = document.querySelector('.tv-series');
+var containerDiv = document.querySelector('.container');
     var movieType = this.value;
     if (movieType === 'movies') {
       moviesList.style.display = '';
