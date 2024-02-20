e5dcf8a5e26d284ad456a64ce9acca84d48e79fa
Chillax Zoom <159781605+chillaxzoom@users.noreply.github.com>
2024-02-20 06:24:54 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,26 @@ const navOpenBtn = document.querySelector("[data-menu-open-btn]");
 const navCloseBtn = document.querySelector("[data-menu-close-btn]");
 const navbar = document.querySelector("[data-navbar]");
 const overlay = document.querySelector("[data-overlay]");
+const navLinks = navbar.querySelectorAll("a"); // Select all anchor links within the navbar
 
 const navElemArr = [navOpenBtn, navCloseBtn, overlay];
 
 for (let i = 0; i < navElemArr.length; i++) {
-
   navElemArr[i].addEventListener("click", function () {
-
     navbar.classList.toggle("active");
     overlay.classList.toggle("active");
     document.body.classList.toggle("active");
-
   });
-
 }
 
+// Add event listeners to each navbar link to remove the 'active' class when clicked
+navLinks.forEach(link => {
+  link.addEventListener("click", function () {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+    document.body.classList.remove("active");
+  });
+});
 
 
 /**
